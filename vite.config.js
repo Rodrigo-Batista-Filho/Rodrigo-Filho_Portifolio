035db8cfa6ef7a70121e091c4f56620e93de5222
digs <batistafilhorodrigo@gmail.com>
2025-05-16 10:29:21 -0300

    corrige base para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Rodrigo-Filho_Portifolio/",
+  base: "/Rodrigo-Filho_Portifolio/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
